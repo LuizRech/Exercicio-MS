9969a3db94cfa060b407106bab0e372ebac61434
LuizRech <luizadre@hotmail.com>
2020-10-21 15:53:37 +0100

    SQL

--- users.sql
+++ users.sql
@@ -1,1 +1,31 @@
-INSERT INTO `users`(`name`, `username`, `password`) VALUES ('luiz', 'luiz', 'luiz');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : exercicio
+ Source Server Type    : MySQL
+ Source Server Version : 50722
+ Source Host           : svr-dev:3306
+ Source Schema         : lrech
+
+ Target Server Type    : MySQL
+ Target Server Version : 50722
+ File Encoding         : 65001
+
+ Date: 21/10/2020 15:52:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
