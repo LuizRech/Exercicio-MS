91ba3d4c38271db21a0390fae63ca65c0a55d78a
LuizRech <luizadre@hotmail.com>
2020-10-22 17:57:28 +0100

    HS MS Aprovado

--- pages/Main/ButtonB/contador.php
+++ pages/Main/ButtonB/contador.php
@@ -3,19 +3,35 @@
 	$sessions = new CheckSession();
 	
 	$nro = $_POST['nro'];
-
+	
 	$string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	$values = array();
 
-	for($i = 0; $i < $nro; $i++){
-			
-		$valToReturn = substr_replace(substr_replace(substr(str_shuffle($string), 0, 10), 'A', 1, 1), 'B', 8, 1);
+	for($count = 0; $count < 1; $count++){
+		for($i = 0; $i < $nro; $i++){
+			$valToReturn = substr_replace(substr_replace(substr(str_shuffle($string), 0, 10), 'A', 1, 1), 'B', 8, 1);
+			$values[$i] = $valToReturn;	
+		}
 
-		if(in_array($valToReturn, $values)){
-				$i--;
-		}else{
-				array_push($values, $valToReturn);
+		$xpto = count(array_unique($values);
+		if($xpto < $nro){
+			$count--;
+			$nro = ($i - $xpto);
 		}
 	}
+
+	/*$array_strings = array();
+	for($i = 0; $i < $nro; $i++){
+		$valToReturn = substr_replace(substr_replace(substr(str_shuffle($string), 0, 10), 'A', 1, 1), 'B', 8, 1);
+		if(isset($array_strings[$valToReturn]))
+		{
+			$i--;
+		}
+		else{
+			$array_strings[$valToReturn] = true;
+		}
+	}*/
+
+
 	exit(json_encode($values));
 ?>
